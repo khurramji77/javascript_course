c62a6f54de07cbcb9c69932faf43587af009e1ea
khurramji77 <khurramjia@hotmail.com>
2024-12-26 01:38:01 -0500

    grocery expenditure

--- calculatedArea/calculate_area.js
+++ calculatedArea/calculate_area.js
@@ -8,3 +8,12 @@ function calculateArea(){
     let area = length * width;
     document.getElementById("result").innerText = `The area of the rectangle is: ${area}`
 }
+
+function groceryTracker(){
+    let grocery1Amt = parseFloat(document.getElementById("grocery1").value);
+    let grocery2Amt = parseFloat(document.getElementById("grocery2").value);
+    let grocery3Amt = parseFloat(document.getElementById("grocery3").value);
+    let totalExpenditure = grocery1Amt + grocery2Amt + grocery3Amt;
+
+    document.getElementById("totalExpenditure").innerText = `The tota amount is: ${totalExpenditure}`
+}
